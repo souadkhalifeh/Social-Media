0f92bec1c18e4a2718bacdce39c56d3b804414f8
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-04-29 18:18:50 +0300

    api

--- api/index.js
+++ api/index.js
@@ -5,8 +5,13 @@ import postRoutes from "./routes/posts.js"
 import likeRoutes from "./routes/likes.js"
 import commentRoutes from "./routes/comments.js"
 import authRoutes from "./routes/auth.js"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser())
 
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
