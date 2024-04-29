d0ca7919d34bdfad49b7b820d6a4d5771194edd6
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-04-29 12:38:30 +0300

    routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { getUser } from "../controllers/user.js";
 
 const router= express.Router()
 
-router.get("/test",)
+router.get("/find/:userId",getUser)
 
 
 export default router
