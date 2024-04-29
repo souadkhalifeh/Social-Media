57660a3eca9cf9c773bc42dde1101dd04836d973
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-04-29 12:30:27 +0300

    js files added

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import Express from "express";
-const app =Express()
+const app =Express();
+import userRoutes from "./routes/users.js"
+
+
+app.use("/api/users",userRoutes)
+
 
 app.listen(3300,()=>{
     console.log("API working!")
